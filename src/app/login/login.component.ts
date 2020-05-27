cbc3af993e28132d87c5605520a810849c460d0f
Asahi <asahimojica@yahoo.com>
2020-05-27 12:33:45 -0500

    hardcoded login component... understanding angular

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = 'Asahi123';
+  password  = '';
+  errorMessage = 'Invalid Credentials';
+  invalidLogin = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  handleLogin() {
+
+    if (this.username === 'Asahi123' && this.password === 'password'){
+      this.invalidLogin =  false;
+
+    } else {
+      this.invalidLogin = true;
+    }
+
+    // console.log(this.username);
+  }
 }
