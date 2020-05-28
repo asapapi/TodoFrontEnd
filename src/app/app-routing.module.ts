6fb33cc3207166c8d238cf2623b8d2eb9e0806f8
Asahi <asahimojica@yahoo.com>
2020-05-28 17:26:34 -0500

    Error Component Created And Routed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {ErrorComponent} from './error/error.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Welcome component
+  {path:  '', component: LoginComponent}, // this sets the default path url to go to the login page with url localhost:4200
+  {path: 'login', component: LoginComponent},
+  {path: 'welcome', component: WelcomeComponent},
+  {path: '**', component: ErrorComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
