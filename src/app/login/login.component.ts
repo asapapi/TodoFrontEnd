c77efbb3d0ce356a2b6d196f30a7da155c4a6cad
Asahi <asahimojica@yahoo.com>
2020-06-29 12:52:46 -0500

    session storage authentication added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {HardcodedAuthenticationService} from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -15,14 +16,16 @@ export class LoginComponent implements OnInit {
 
   // Inject Router
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
 
   ngOnInit() {
   }
 
   handleLogin() {
 
-    if (this.username === 'Asahi123' && this.password === 'password') {
+    // @ts-ignore
+    if (this.hardcodedAuthenticationService.authenticate(this.username, this.password)) {
       this.router.navigate(['welcome', this.username]);
       this.invalidLogin =  false;
 
